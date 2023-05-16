01ad9f8441b39b82bca1960c098b21425796b7cb
Ranjeet Singh <ranjeetsinghfattu@gmail.com>
2023-05-16 18:05:58 +0530

    Basic calculator is ready with calculation history

--- calculator.js
+++ calculator.js
@@ -1,21 +1,38 @@
 document.querySelectorAll(".txt").forEach(button_click);
 
-function button_click(value){
-value.addEventListener('click',()=>{
-    let symbol=value.innerHTML;
-    let prev =document.querySelector('.ot').innerHTML;
-    document.querySelector('.ot').innerHTML=prev+symbol;
+function button_click(val){
+val.addEventListener('click',()=>{
+    let symbol=val.innerHTML;
+    let prev =document.querySelector('.ot').value;
+    document.querySelector('.ot').value=prev+symbol;
     if(symbol=='AC'){
-        document.querySelector('.ot').innerHTML='';
+        document.querySelector('.ot').value='';
     }
     else if(symbol=='X'){
-        document.querySelector('.ot').innerHTML=prev.slice(0,prev.length-1);
+        document.querySelector('.ot').value=prev.slice(0,prev.length-1);
     }
     else if(symbol=="="){
-
+        try {
+            let newnode=document.createElement('li');
+            let textnode=document.createTextNode(prev+"="+eval(prev));
+            newnode.appendChild(textnode);
+            let list=document.querySelector('.anslist');
+            if (list.children.length==0) {
+                document.querySelector('.ot').value=eval(prev);
+                list.appendChild(newnode);
+            } else {
+                list.insertBefore(newnode,list.children[0]);
+                document.querySelector('.ot').value=eval(prev);
+            }
+            
+        } catch (error) {
+            document.querySelector('.ot').value="";
+            alert("Invaild expression")
+        }
+        
     }
     else{
-        document.querySelector('.ot').innerHTML=prev+symbol;
+        document.querySelector('.ot').value=prev+symbol;
     }
 
 });
