5ab9885eff53bcbff70dac821b967ee0e41d66bb
Ranjeet Singh <ranjeetsinghfattu@gmail.com>
2023-05-20 18:07:00 +0530

    make it responsive with bootstrap classes

---
+++ README.md
@@ -0,0 +1,2 @@
+Learning in this projects are:-
+1. array.slice(start,end)  //End is not inclusive
--- calculator.js
+++ calculator.js
@@ -1,119 +1,78 @@
-document.querySelectorAll(".txt").forEach(button_click);
+button_list = document.querySelectorAll(".txt")
+button_list.forEach(button_click);
 
-function button_click(val){
-val.addEventListener('click',()=>{
-    let symbol=val.innerHTML;
-    let prev =document.querySelector('.ot').value;
-    
-    if(symbol=='AC'){
-        document.querySelector('.ot').value='';
-    }
-    else if(symbol=='X'){
-        document.querySelector('.ot').value=prev.slice(0,prev.length-1);
-    }
-    else if(symbol=="="){
-        try {
-            let newnode=document.createElement('li');
-            let textnode=document.createTextNode(prev+"="+eval(prev));
-            newnode.appendChild(textnode);
-            let list=document.querySelector('.anslist');
-            if (list.children.length==0) {
-                document.querySelector('.ot').value=eval(prev);
-                list.appendChild(newnode);
-            } else {
-                list.insertBefore(newnode,list.children[0]);
-                document.querySelector('.ot').value=eval(prev);
-            }
-            
-        } catch (error) {
-            document.querySelector('.ot').value="";
-            alert("Invaild expression")
+function button_click(val) {   //adding event listner to each button 
+    val.addEventListener('click', () => {
+        let symbol = val.innerHTML;
+        let prev = document.querySelector('.ot').value;
+
+        if (symbol == 'AC') {
+            document.querySelector('.ot').value = '';
         }
-        
-    }
-    else{
-        document.querySelector('.ot').value=prev+symbol;
-    }
+        else if (symbol == 'X') {
+            document.querySelector('.ot').value = prev.slice(0, prev.length - 1);
+        }
+        else if (symbol == "=") {
+            try {
+                let newnode = document.createElement('li');
+                newnode.innerText=`${prev} = ${eval(prev)}`;
+                let list = document.querySelector('.anslist');
+                if (list.children.length == 0) {
+                    document.querySelector('.ot').value = eval(prev);
+                    list.appendChild(newnode);
+                } else {
+                    list.insertBefore(newnode, list.children[0]);
+                    document.querySelector('.ot').value = eval(prev);
+                }
 
-});
+            } catch (error) {
+                document.querySelector('.ot').value = "";
+                alert("Invaild expression")
+            }
+
+        }
+        else {
+            document.querySelector('.ot').value = prev + symbol;
+        }
+
+    });
 }
 
-document.addEventListener('keydown',evt=>{
-    let key=evt.key;
-    let prev =document.querySelector('.ot').value;
-    if(key==='Delete'){
-        document.querySelector('.ot').value='';
+document.addEventListener('keydown', evt => { //adding event listner to each keypress
+    let key = evt.key;
+    let prev = document.querySelector('.ot').value;
+    if (key === 'Delete') {
+        document.querySelector('.ot').value = '';
     }
-    else if(key=='Backspace'){
-        document.querySelector('.ot').value=prev.slice(0,prev.length-1);
+    else if (key == 'Backspace') {
+        document.querySelector('.ot').value = prev.slice(0, prev.length - 1);
     }
-    else if(key==="="){
-        if(eval(prev)){
-        try {
-            let newnode=document.createElement('li');
-            let textnode=document.createTextNode(prev+"="+eval(prev));
-            newnode.appendChild(textnode);
-            let list=document.querySelector('.anslist');
-            if (list.children.length==0) {
-                document.querySelector('.ot').value=eval(prev);
-                list.appendChild(newnode);
-            } else {
-                list.insertBefore(newnode,list.children[0]);
-                document.querySelector('.ot').value=eval(prev);
-            }
-            
-        } catch (error) {
-            document.querySelector('.ot').value="";
-            alert("Invaild expression")
-        }}
-        
-    }
-    else if(key in ['1','2','3','4','5','6','7','8','9','0']){
-        document.querySelector('.ot').value=prev+key;
-    }
-    else if (key=='.' || key=='+' ||key=='-' ||key=='*' ||key=='/' ||key=='(' ||key==')' ) {
-        document.querySelector('.ot').value=prev+key;
-    }
-    // if (key==='Delete') {
-    //     document.querySelector('.ot').value='';
-    // }
-    // else if (key==='Backspace') {
-    //     document.querySelector('.ot').value=prev.slice(0,prev.length-1);
-    // }
-    // else if(key==="="){
-    //     try {
-    //         let newnode=document.createElement('li');
-    //         let textnode=document.createTextNode(prev+"="+eval(prev));
-    //         newnode.appendChild(textnode);
-    //         let list=document.querySelector('.anslist');
-    //         if (list.children.length==0) {
-    //             document.querySelector('.ot').value=eval(prev);
-    //             list.appendChild(newnode);
-    //         } else {
-    //             list.insertBefore(newnode,list.children[0]);
-    //             document.querySelector('.ot').value=eval(prev);
-    //         }
-            
-    //     } catch (error) {
-    //         document.querySelector('.ot').value="";
-    //         alert("Invaild expression")
-    //     }
-        
-    // }
-    // else {
-        
-    // }
-})
+    else if (key === "=") {
+        if (eval(prev)) {
+            try {
+                let newnode = document.createElement('li');
+                let textnode = document.createTextNode(prev + "=" + eval(prev));
+                newnode.appendChild(textnode);
+                let list = document.querySelector('.anslist');
+                if (list.children.length == 0) {
+                    document.querySelector('.ot').value = eval(prev);
+                    list.appendChild(newnode);
+                } else {
+                    list.insertBefore(newnode, list.children[0]);
+                    document.querySelector('.ot').value = eval(prev);
+                }
 
-// function add_to_output(value){
-//     console.log(value);
-//     let symbol=value.innerHTML;
-//     let prev =document.querySelector('.ot').innerHTML;
-//     document.querySelector('.ot').innerHTML=prev+symbol;
-// }
-// document.querySelectorAll('.txt').forEach((value)=>{console.log(value);}) 
-// for (let i = 0; i <19; i++) {
-//     document.querySelectorAll(".txt")[i].addEventListener('click',button_click);
-// }
-// document.querySelector(".txt").addEventListener('click',button_click);
-// document.add
+            } catch (error) {
+                document.querySelector('.ot').value = "";
+                alert("Invaild expression")
+            }
+        }
+
+    }
+    else if (key in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']) {
+        document.querySelector('.ot').value = prev + key;
+    }
+    else if (key == '.' || key == '+' || key == '-' || key == '*' || key == '/' || key == '(' || key == ')') {
+        document.querySelector('.ot').value = prev + key;
+    }
+})
